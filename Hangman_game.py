1361b9e704cd03e131d1d4e1fd87254a44662855
Monisha <bmonisaa9@gmail.com>
2024-08-08 15:44:09 +0200

    second_stage_changes

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,6 +1,63 @@
 import random
 
-word_list = ["testimony", "revere", "encapsulation"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["program", "revere", "henry"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -13,19 +70,32 @@ print(placeholder)
 
 game_start = True  #start the game
 stored = []
+lives = 6
 
 while game_start :
-         guess = input("Guess a letter: ").lower()
-         if guess not in stored:
+        guess = input("Guess a letter: ").lower()
+        if guess not in stored:
                 stored.append(guess)
-         display = ""
-         for letter in chosen_word:
-             if letter in stored:
-                 display += letter
-             else:
+        display = ""
+        for letter in chosen_word:
+            if letter in stored:
+                display += letter
+            else:
                 display += "_"
-         new_display = display
-         print(new_display)
-         if not "_" in display:
-            game_start= False
-            print("won")
+        new_display = display
+        print(new_display)
+
+            
+        if guess not in chosen_word:
+            print(stages[lives - 1])
+            lives -= 1
+            
+        elif guess in chosen_word :
+            print(stages[lives])
+            
+        if "_" not in new_display:
+            game_start = False
+            print("You win.")
+        if lives == 0:
+            game_start = False
+            print("You lose")
