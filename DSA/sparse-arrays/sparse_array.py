f3a8a8d691c26817a52803590017c4f424104724
Monisha <bmonisaa9@gmail.com>
2024-08-14 23:36:18 +0200

    final commit

---
+++ DSA/sparse-arrays/sparse_array.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    # Write your code here
+    result = []
+    for items1 in queries:
+        count = 0
+        for items2 in stringList:
+            if items1 == items2:
+                count+=1
+        result.append(count)
+    return(result)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
