5122703d54aac9fc69a7b1a094f54175a625a537
Monisha <bmonisaa9@gmail.com>
2024-08-19 19:30:44 +0200

    initial commit

---
+++ DSA/stack/max_element/max_ele.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getMax' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY operations as parameter.
+#
+
+def getMax(operations):
+    # Write your code here
+    result = []
+    for item in operations:
+        temp = []
+        for new in item.split():
+            temp.append(int(new))
+        result.append(temp)
+        
+    answer = []
+
+    for element in result:
+
+        if element[0] == 1:
+            answer.append(element[1])
+        elif element[0] == 3:
+            max(answer)
+        elif element[0] == 2:
+            answer.pop()
+        
+    return(answer)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ops = []
+
+    for _ in range(n):
+        ops_item = input()
+        ops.append(ops_item)
+
+    res = getMax(ops)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+''
